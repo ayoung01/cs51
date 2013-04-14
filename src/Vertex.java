c17f828c311f1f6b58b719e4ea6fb4e4f8fdb785
Albert Young <ayoung01@college.harvard.edu>
2013-04-14 11:57:29 -0400

    why so many interfaces...

---
+++ src/Vertex.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ayoung
+ * Date: 4/14/13
+ * Time: 11:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Vertex implements Vertex_I {
+    /* Do we want to add instance variables such that the vertex occupies some position in an n-dimensional space? */
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public String toString() {
+        return Integer.toString(id);
+    }
+}
