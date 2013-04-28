b71347d2486ab88e5d818f78843f11cf1c566b15
Brian <brianshin22@hotmail.com>
2013-04-28 03:44:39 -0400

    Merge branch 'master' of C:\Users\Brian\Dropbox\CS51\Final Project with conflicts.

--- src/Genetic.java
+++ src/Genetic.java
@@ -8,10 +8,21 @@ import java.util.Random;
  * To change this template use File | Settings | File Templates.
  */
 
+// resources: http://www.codeproject.com/Articles/1403/Genetic-Algorithms-and-the-Traveling-Salesman-Prob
+//  http://www.ai-junkie.com/ga/intro/gat2.html
+//  http://jgap.sourceforge.net/javadoc/3.01/org/jgap/impl/GreedyCrossover.html
+// http://www.theprojectspot.com/tutorial-post/creating-a-genetic-algorithm-for-beginners/3
 
 public class Genetic implements TSP_I{
 
     private Random rand;
+
+    private final double mutationRate = 0.03;
+    private int popSize;
+
+    public Genetic (int popSize) {
+        popSize = popSize;
+    }
 
     public Tour findShortestPath(Graph g) {
         Tour[] population = generatePopulation(g);
@@ -19,6 +30,10 @@ public class Genetic implements TSP_I{
         // to be crossbred, with some chance of mutation, and generate a next generation population.
         // continue for a specified number of times, then return the current most fit tour in the population, which
         // will hopefully be a very close approximation to the real solution.
+
+        // at each generation, pick two parents and create two new child tours (perform two crossovers)
+        // and replace the two parents in the population
+
 
 
 
@@ -32,7 +47,7 @@ public class Genetic implements TSP_I{
     }
 
     private Tour[] generatePopulation (Graph g) {
-        int initialPopulationSize = g.numVertices();
+        int initialPopulationSize = popSize;
         Tour[] population = new Tour[initialPopulationSize];
         for (int i=0;i<initialPopulationSize;i++) {
             population[i] = g.getRandomTour();
@@ -41,15 +56,18 @@ public class Genetic implements TSP_I{
     }
 
     private Tour[] rankFitness(Tour[] population) {
+
         return null;
     }
 
     // http://jgap.sourceforge.net/javadoc/3.01/org/jgap/impl/GreedyCrossover.html
     // select randomly a starting city from one parent. Then compare
-    public Tour greedyCrossover (Tour a, Tour b) {
+    public Tour greedyCrossover (Tour a, Tour b, Graph g) {
         Tour child;
         Vertex[] vertices1,vertices2;
         Edge[] edges1, edges2;
+        int length;
+
         if (rand.nextInt(2) == 0) {
             vertices1 = a.verticesSoFar();
             edges1 = a.allEdges();
@@ -62,30 +80,97 @@ public class Genetic implements TSP_I{
             vertices2 = a.verticesSoFar();
             edges2 = b.allEdges();
         }
-        Vertex start = vertices1[0];
-        child = new Tour(start, vertices1.length);
+
+        // vertices1 and vertices2 should always be the same length - invariant of population of tours
+        length = vertices1.length;
+        child = new Tour(vertices1[0], length);
+
+        int pos1, pos2;
+        int nextPos1, nextPos2;
 
         // while the child tour is not complete
-        while (child.verticesSoFar().length < vertices1.length) {
-            if (edges1[0].getWeight() <= edges2[indexOfVertex(vertices2,start)].getWeight()) {
-                child.addEdge(edges1[0]);
+        while (child.verticesSoFar().length < length) {
+            // position of the vertex at each respective vertex array
+            pos1 = indexOfVertex(vertices1, child.getCurrentVertex());
+            pos2 = indexOfVertex(vertices2, child.getCurrentVertex());
+
+            // if both are valid vertices, look at respective next vertices and compare
+            if (pos1 == length-1) {
+                nextPos1 = 0;
             }
             else {
-                child.addEdge(edges2[0]);
+                nextPos1 = pos1+1;
             }
+            if (pos2 == length-1) {
+                nextPos2 = 0;
+            }
+            else {
+                nextPos2= pos2+1;
+            }
+
+            // Four cases: both next vertices are already in child, one of next vertices is in child, neither next
+            // vertices are in child
+
+            // if both next vertices are already in the child tour, then select a random vertex as next vertex
+            if (child.containsVertex(vertices1[nextPos1]) && child.containsVertex(vertices2[nextPos2])) {
+                // pick a random vertex array to pull vertex from
+                int pickOne = rand.nextInt(2);
+                Vertex[] randTour;
+                if (pickOne == 0) {
+                    randTour = vertices1;
+                }
+                else randTour = vertices2;
+
+                // pick random vertex to connect to
+                int next = rand.nextInt(length);
+                while (child.containsVertex(randTour[next])) {
+                    next = rand.nextInt(length);
+                }
+                // add this random vertex to the tour
+                child.addEdge(g.edgeBetween(child.getCurrentVertex(),randTour[next]));
+            }
+            // if the child tour contains the next vertex of the first tour and NOT the vertex of second tour
+            else if (child.containsVertex(vertices1[nextPos1])) {
+                child.addEdge(edges2[pos2]);
+            }
+            // if the child tour contains the next vertex of the second tour and NOT of the first tour
+            else if (child.containsVertex(vertices2[nextPos2])) {
+                child.addEdge(edges1[pos1]);
+            }
+            // neither next vertex is in the child tour, so take the closer one
+            else {
+                if (edges1[pos1].getWeight() <= edges2[pos2].getWeight()) {
+                    child.addEdge(g.edgeBetween(child.getCurrentVertex(),vertices1[nextPos1]));
+                }
+                else child.addEdge(g.edgeBetween(child.getCurrentVertex(),vertices2[nextPos2]));
+            }
+            System.out.println("The num of vertices of the tours passed in were: Tour a = " + vertices1.length +
+                    " Tour b = " + vertices2.length
+                    + " The num of vertices of the child tour is " + child.verticesSoFar().length);
+
         }
-        return null;
+        return child;
     }
 
-    public Tour mutate (Tour a) {
-        return null;
+    public Tour mutate (Tour a, Graph g) {
+        Tour mutated;
+        int index1 = rand.nextInt(a.verticesSoFar().length);
+        int index2 = rand.nextInt(a.verticesSoFar().length);
+        Vertex[] swappedVertices = a.verticesSoFar()
+        Vertex temp = swappedVertices[index1];
+        swappedVertices[index1] = a.verticesSoFar()[index2];
+        swappedVertices[index2] = temp;
+        return new Tour(swappedVertices,g);
     }
 
     private int indexOfVertex(Vertex[] vertices, Vertex a) {
         int i = 0;
-        while (vertices[i].getId() != a.getId()) {
+        while (vertices[i].getId() != a.getId() && i<vertices.length) {
             i++;
         }
-        return i;
+        if (i == vertices.length) {
+            return -1;
+        }
+        else return i;
     }
 }
