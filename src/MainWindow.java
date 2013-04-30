2e3e829c79206fc9c74be47ced9f40ccaf99dbc1
Brian <brianshin22@hotmail.com>
2013-04-30 17:50:48 -0400

    GUI of graph and tour

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -39,7 +39,32 @@ public class MainWindow extends JPanel
         System.out.printf("Simulated Annealing: %.2f in %.5f s\n ", bestsim.getLength(), duration);
         bestsim.printGraphToFile("simbest.txt");
 
-        MainWindow panel = new MainWindow(g, bestsim);                            // window for drawing
+        TwoOpt twoopt = new TwoOpt();
+        startTime = System.nanoTime();
+        Tour besttwoopt = twoopt.findShortestPath(g);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime)/1000000000.0;
+        System.out.printf("Two Opt: %.2f in %.5f s\n", besttwoopt.getLength(), duration);
+        besttwoopt.printGraphToFile("twooptbest.txt");
+
+        Greedy greedisgood = new Greedy();
+        startTime = System.nanoTime();
+        Tour bestgreedy = greedisgood.findShortestPath(g);
+        endTime = System.nanoTime();
+        duration = (endTime - startTime)/1000000000.0;
+        System.out.printf("Greedy: %.2f in %.5f s\n", bestgreedy.getLength(), duration);
+        bestgreedy.printGraphToFile("Greedybest.txt");
+
+        Genetic genetic = new Genetic(100,30000);
+        startTime = System.nanoTime();
+        Tour bestgenetic = genetic.findShortestPath(g);
+        endTime = System.nanoTime();
+        duration = (endTime-startTime)/1000000000.0;
+        System.out.printf("Genetic: %.2f in %.5f s\n", bestgenetic.getLength(),duration);
+        bestgenetic.printGraphToFile("Geneticbest.txt");
+
+
+        MainWindow panel = new MainWindow(g, bestgenetic);                            // window for drawing
         JFrame application = new JFrame("Graph");                            // the program itself
 
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // set frame to exit
@@ -64,8 +89,10 @@ public class MainWindow extends JPanel
     {
         int width = getWidth();             // width of window in pixels
         int height = getHeight();           // height of window in pixels
+        int radius = 2;
 
         EuclideanVertex[] vertices = graph.getVertices();
+        int length = vertices.length;
         double[] mins = graph.getMins();
         double[] maxes = graph.getMaxes();
 
@@ -73,27 +100,37 @@ public class MainWindow extends JPanel
         super.paintComponent(g);            // call superclass to make panel display correctly
 
         Graphics2D g2d = (Graphics2D) g;
-        g.drawString("Hello", 475 , 375 );
+        //g.drawString("Hello", 475 , 375 );
         g2d.setColor(Color.blue);
 
-        drawGraph(graph);
-        drawTour();
+        Point[] points = new Point[length];
 
         for(int i=0;i<vertices.length;i++) {
-            System.out.println("x1 = " + vertices[i].getCoord(0));
-            System.out.println("x2 = " + vertices[i].getCoord(1));
-            int pos1 = (int) (Math.abs((vertices[i].getCoord(0) - mins[0])/(maxes[0]-mins[0]))*(4*width/5));
-            int pos2 = (int) (Math.abs((vertices[i].getCoord(1) - mins[1])/(maxes[1]-mins[1]))*(4*height/5));
-            System.out.printf("Mins: %.2f,%.2f Maxes: %.2f, %.2f\n", mins[0],mins[1],maxes[0],maxes[1]);
+            int currentVertexId = besttour.verticesSoFar()[i].getId();
+            int pos1 = (int) (((vertices[currentVertexId].getCoord(0) - mins[0])/(maxes[0]-mins[0]))*(4*width/5) + (width/20));
+            int pos2 = (int) (((vertices[currentVertexId].getCoord(1) - mins[1])/(maxes[1]-mins[1]))*(4*height/5) + (height/20));
+            points[i] = new Point(pos1,pos2);
+        }
 
-            Ellipse2D.Double circle = new Ellipse2D.Double(pos1+(width/20), pos2+(height/20), 5, 5);
+        for(int i=0;i<length;i++) {
+            //System.out.println("x1 = " + points[i].getX());
+            //System.out.println("x2 = " + points[i].getY());
+            //System.out.printf("Mins: %.2f,%.2f Maxes: %.2f, %.2f\n", mins[0],mins[1],maxes[0],maxes[1]);
+
+            Ellipse2D.Double circle = new Ellipse2D.Double(points[i].getX(), points[i].getY(), 2*radius, 2*radius);
             g2d.fill(circle);
         }
+
+        for(int i=0;i<length;i++) {
+            g2d.drawLine(points[i].getX()+radius,points[i].getY()+radius,points[(i+1)%length].getX()+radius,points[(i+1)%length].getY()+radius);
+        }
+
 
 
 
         // Drawing code goes here
     }
+
 
 
 }
