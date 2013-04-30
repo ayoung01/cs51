b0d2083a03ef3ff62b3a6380d02ce140dd1e86be
mgentili <mgentili@gmail.com>
2013-04-30 15:38:05 -0400

    Waiting

---
+++ src/Graphics2Dim.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mgentili
+ * Date: 4/30/13
+ * Time: 3:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.awt.Graphics;
+
+public class Graphics2Dim {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
